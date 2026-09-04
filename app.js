1059639eb67377a5c0e4e7e88f5438d7494c2010
bahaa/bbaa44x <lybha8943@gmail.co>
2026-09-04 08:50:09 +0300

    modiged app.js fixed the port

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cors({
 }))
 app.use(express.json());
 app.use(cookieParser())
-app.listen(3000,"localhost",()=>
+app.listen(process.env.PORT ?? 3000,"localhost",()=>
 {
     console.log("server is running on 3000");
 })
